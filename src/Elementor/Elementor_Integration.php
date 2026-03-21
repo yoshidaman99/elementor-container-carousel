22fc4513e383aff7cb1cdf6d79a1b7e8397f3552
yoshidaman99 <yoshidaman99@users.noreply.github.com>
2026-03-22 07:07:23 +0800

    Debug v1.0.6 - Scan Elementor classes for correct container class

--- src/Elementor/Elementor_Integration.php
+++ src/Elementor/Elementor_Integration.php
@@ -10,20 +10,34 @@ class Elementor_Integration
 {
     public function register_widgets($widgets_manager): void
     {
-        error_log('[ECC] Registering widgets - Elementor: ' . (defined('ELEMENTOR_VERSION') ? ELEMENTOR_VERSION : 'not loaded'));
+        error_log('[ECC] Elementor: ' . (defined('ELEMENTOR_VERSION') ? ELEMENTOR_VERSION : 'not loaded'));
 
-        if (class_exists('\Elementor\Widget_Container', true)) {
-            error_log('[ECC] Widget_Container class found');
-            try {
-                require_once ECC_DIR . 'src/Elementor/Widgets/Container_Carousel_Widget.php';
-                $widget = new Widgets\Container_Carousel_Widget();
-                $widgets_manager->register($widget);
-                error_log('[ECC] Container_Carousel_Widget registered successfully');
-            } catch (\Throwable $e) {
-                error_log('[ECC] Container_Carousel_Widget FAILED: ' . $e->getMessage() . ' in ' . $e->getFile() . ':' . $e->getLine());
+        $possible_classes = [
+            'Elementor\Widget_Container',
+            'Elementor\Element_Container',
+            'Elementor\Elements\Container',
+            'Elementor\Core\Elements\Container',
+            'Elementor\Modules\Container\Container',
+            'Elementor\Includes\Elements\Container',
+        ];
+        foreach ($possible_classes as $cls) {
+            if (class_exists($cls, true)) {
+                error_log('[ECC] FOUND: ' . $cls);
+                $parent = $cls;
+                while ($parent = get_parent_class($parent)) {
+                    error_log('[ECC]   extends: ' . $parent);
+                }
+                $ref = new \ReflectionClass($cls);
+                $methods = array_map(function ($m) { return $m->getName(); }, $ref->getMethods(\ReflectionMethod::IS_PUBLIC | \ReflectionMethod::IS_ABSTRACT));
+                error_log('[ECC]   public methods: ' . implode(', ', $methods));
             }
-        } else {
-            error_log('[ECC] Widget_Container class NOT found');
+        }
+
+        error_log('[ECC] Widget_Container class NOT found - listing Elementor\\ classes with "container" in name');
+        foreach (get_declared_classes() as $cls) {
+            if (stripos($cls, 'Elementor') !== false && stripos($cls, 'container') !== false) {
+                error_log('[ECC]   ' . $cls);
+            }
         }
 
         require_once ECC_DIR . 'src/Elementor/Widgets/Slides_Carousel_Widget.php';
