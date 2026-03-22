059e78300f6706823cd3431878a217511bd530a4
yoshidaman99 <yoshidaman99@users.noreply.github.com>
2026-03-22 22:13:59 +0800

    Release v1.1.19 - Fix PHP fatal error from corrupted element data on frontend

--- src/Core/Plugin.php
+++ src/Core/Plugin.php
@@ -32,6 +32,8 @@ class Plugin
         add_action('elementor/editor/after_enqueue_scripts', [\Elementor_Container_Carousel\Elementor\Elementor_Integration::class, 'enqueue_editor_assets']);
         add_action('elementor/frontend/after_register_styles', [\Elementor_Container_Carousel\Elementor\Elementor_Integration::class, 'register_styles']);
         add_action('elementor/frontend/after_register_scripts', [\Elementor_Container_Carousel\Elementor\Elementor_Integration::class, 'register_scripts']);
+
+        add_filter('get_post_metadata', [$this, 'filter_elementor_data'], 10, 4);
     }
 
     public function load_textdomain(): void
@@ -65,5 +67,56 @@ class Plugin
 
         $integration = new \Elementor_Container_Carousel\Elementor\Elementor_Integration();
         $integration->register_widgets($widgets_manager);
+    }
+
+    private static array $sanitized_cache = [];
+    private static bool $filtering = false;
+
+    public function filter_elementor_data($check, $object_id, $meta_key, $single)
+    {
+        if ($meta_key !== '_elementor_data' || is_admin() || wp_doing_ajax() || self::$filtering) {
+            return $check;
+        }
+
+        if (isset(self::$sanitized_cache[$object_id])) {
+            $data = self::$sanitized_cache[$object_id];
+            return $single ? $data : [$data];
+        }
+
+        self::$filtering = true;
+        remove_filter('get_post_metadata', [$this, 'filter_elementor_data']);
+
+        $raw_data = get_post_meta($object_id, $meta_key, true);
+
+        add_filter('get_post_metadata', [$this, 'filter_elementor_data'], 10, 4);
+        self::$filtering = false;
+
+        if (!is_array($raw_data)) {
+            return $check;
+        }
+
+        $cleaned = $this->sanitize_elements($raw_data);
+        self::$sanitized_cache[$object_id] = $cleaned;
+
+        return $single ? $cleaned : [$cleaned];
+    }
+
+    private function sanitize_elements(array $elements): array
+    {
+        $result = [];
+
+        foreach ($elements as $element) {
+            if (isset($element['elType']) && $element['elType'] === 'widget' && empty($element['widgetType'])) {
+                continue;
+            }
+
+            if (isset($element['elements']) && is_array($element['elements'])) {
+                $element['elements'] = $this->sanitize_elements($element['elements']);
+            }
+
+            $result[] = $element;
+        }
+
+        return $result;
     }
 }
